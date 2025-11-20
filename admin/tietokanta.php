338ef4b1ab56badb8ec5df467d39f02fbe3f07d9
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-11-20 09:02:53 +0200

    Update tietokanta.php

--- admin/tietokanta.php
+++ admin/tietokanta.php
@@ -10,7 +10,6 @@
 		name VARCHAR(255) NOT NULL, 
     	email VARCHAR(255) NOT NULL,
     	message VARCHAR(4000) NOT NULL,
-		maybe_spam TINYINT(1) DEFAULT 0,
-		ip_address VARCHAR(45));"
+		maybe_spam TINYINT(1) DEFAULT 0);"
 	);
 ?>
