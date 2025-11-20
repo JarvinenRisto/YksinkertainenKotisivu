e6a2f8a94a0c447bb405ea5fae72360c6ef7fece
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-11-20 19:15:52 +0200

    Update laheta.php

--- laheta.php
+++ laheta.php
@@ -95,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 			$ehkaRoskapostia = 1;
 		}
 
-		$lauseke = $sql->prepare("INSERT INTO Kuntokeskus_viestit (nimi, sposti, viesti, ehkaRoskapostia, ip_osoite) VALUES (?, ?, ?, ?, ?)")
+		$lauseke = $sql->prepare("INSERT INTO Kuntokeskus_viestit (nimi, sposti, viesti, ehkaRoskapostia, ip_osoite) VALUES (?, ?, ?, ?, ?)");
 
 		if ($lauseke->execute([$nimi, $email, $viesti, $ehkaRoskapostia, $ipOsoite])) {
 			$lauseke->close();
