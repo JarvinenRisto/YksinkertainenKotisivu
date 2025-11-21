21e46da60413003619af06cc4edb2134f0805a18
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-11-21 09:33:32 +0200

    Update laheta.php

--- laheta.php
+++ laheta.php
@@ -121,7 +121,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 		$email  = trim($_POST['email'] ?? '');
 		$viesti = trim($_POST['viesti'] ?? '');
 
-		$pituus = strlen($viesti);
+		$pituus = mb_strlen($viesti, 'UTF-8');;
 
 		if ($pituus < 10) {
 			die("Alle 10 merkin pituinen viesti.");
