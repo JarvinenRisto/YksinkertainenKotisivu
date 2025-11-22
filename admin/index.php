e72bd4dd388aa8baa6e18f5965e94cd0b6b5e607
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-11-22 13:55:01 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -35,6 +35,7 @@
 <html lang="fi"><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../teema.css">
     <title>Kuntospurtti – Ylläpito</title>
 </head>
