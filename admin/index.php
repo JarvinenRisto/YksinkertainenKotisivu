a72209b979fbaf249b101abb7c3c91c944cb6490
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-11-30 08:02:32 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@
 
 	$nonce = base64_encode(random_bytes(16));
 
-	header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$nonce' 'unsafe-hashes'; style-src 'self'; img-src 'self'; connect-src 'self'; frame-ancestors 'none';");
+	header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$nonce'; style-src 'self'; img-src 'self'; connect-src 'self'; frame-ancestors 'none';");
 
 	session_set_cookie_params([
 	  'lifetime' => 0,
