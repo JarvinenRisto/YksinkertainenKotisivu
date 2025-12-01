20484f117d4b8818660421f8d1eccedabf3ccfc1
Risto Järvinen <162967474+JarvinenRisto@users.noreply.github.com>
2025-12-01 14:23:12 +0200

    Update laheta.php

--- laheta.php
+++ laheta.php
@@ -197,8 +197,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 		
 		if ($lauseke->execute()) {
 		    $lauseke->close();
-		    header("Refresh: 2; URL=index.html");
-		    echo 'Lomakkeen tiedot lähetetty, sinut ohjataan takaisin etusivulle 2 sekunnin kuluttua.';
+		    header("Refresh: 3; URL=index.html");
+		    echo 'Lomakkeen tiedot lähetetty, sinut ohjataan takaisin etusivulle 3 sekunnin kuluttua.';
 		    exit;
 		} else {
 		    $lauseke->close();
